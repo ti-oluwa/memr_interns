bb60389d3824cc875504e2b66c59f526104cee4d
tioluwa <tioluwa.dev@gmail.com>
2025-01-22 21:33:35 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 pip install poetry
 poetry init -n
-poetry install
+poetry install --no-root
 python3 manage.py collectstatic --noinput
 python3 manage.py generateimages
 python3 manage.py migrate
